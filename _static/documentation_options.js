000d46d80177e9fcfa6ce5b1253dba1979b4c313
Marcus Schäfer <ms@suse.de>
2021-08-10 13:21:23 +0000

    Update GitHub Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9.23.49',
+    VERSION: '9.23.50',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
