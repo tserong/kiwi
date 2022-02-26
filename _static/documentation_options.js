0d51e7c77ec1eea1ce6c16a18ef0c6bce4e67a57
schaefi <schaefi@users.noreply.github.com>
2022-02-26 19:49:18 +0000

    deploy: 914f8611cec031386d34f46c30e7df0249faa9bb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9.24.24',
+    VERSION: '9.24.25',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
